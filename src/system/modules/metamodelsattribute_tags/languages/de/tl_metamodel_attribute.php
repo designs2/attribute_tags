f1e676440c5f35dab3e5ca044ff4f301956121d3
Mini Model <minimodel@metamodel.me>
2013-05-13 23:09:29 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_tags/languages/de/tl_metamodel_attribute.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:13:15+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']      = 'Einstellungen anzeigen';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']           = 'Die SQL-Abfrage verursacht einen Fehler.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['0']      = 'Schlagwortalias';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['1']      = 'Bitte wählen Sie einen Eintrag für den Tag Alias aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['0']     = 'Tabellenspalte';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['1']     = 'Bitte wählen Sie eine Spalte.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['0']         = 'Schlagwort ID';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['1']         = 'Bitte wählen Sie einen Eintrag für die Tag ID aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']    = 'Schlagwortsortierung';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']    = 'Bitte wählen Sie einen Eintrag für die Tag-Sortierung aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['0']      = 'Datenbanktabelle';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['1']      = 'Bitte wählen Sie eine Datenbanktabelle aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0']      = 'SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1']      = 'Die Auswahlmöglichkeiten können per SQL eingeschränkt werden.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags'] = 'Schlagwort';
