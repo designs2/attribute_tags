54332554f405dc130ef0f22adf77655a58b35b29
Mini Model <minimodel@metamodel.me>
2013-06-23 16:14:03 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_tags/languages/it/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_tags/languages/it/tl_metamodel_attribute.php
@@ -8,8 +8,10 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T07:58:40+02:00
+ * last-updated: 2013-06-20T20:14:41+02:00
  */
 
-
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend'] = 'Impostazioni di visualizzazione';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']      = 'La query SQL causa un errore.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0'] = 'SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1'] = 'La lista di opzioni può essere limitata utilizzando SQL.';
