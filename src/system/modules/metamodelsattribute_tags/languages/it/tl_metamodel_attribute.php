30cacc91d4be05e421ce1474b138f902e6aa6acc
Mini Model <minimodel@metamodel.me>
2013-07-22 12:44:05 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_tags/languages/it/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_tags/languages/it/tl_metamodel_attribute.php
@@ -8,10 +8,21 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-06-20T20:14:41+02:00
+ * last-updated: 2013-07-10T10:05:31+02:00
  */
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend'] = 'Impostazioni di visualizzazione';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']      = 'La query SQL causa un errore.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0'] = 'SQL';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1'] = 'La lista di opzioni può essere limitata utilizzando SQL.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']      = 'Impostazioni di visualizzazione';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']           = 'La query SQL causa un errore.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['0']      = 'Alias Tag';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['1']      = 'Selezionare un valore per l\'alias del tag.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['0']     = 'Colonna tabella';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['1']     = 'Selezionare le colonna della tabella. ';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['0']         = 'Tag ID';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['1']         = 'Selezionare un valore per il Tag ID.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']    = 'Ordinamento tag';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']    = 'Selezionare il valore per l\'ordinamento del tag.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['0']      = 'Tabella del database';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['1']      = 'Selezionare la tabella del database.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0']      = 'SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1']      = 'La lista di opzioni può essere limitata utilizzando SQL.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags'] = 'Tags';
