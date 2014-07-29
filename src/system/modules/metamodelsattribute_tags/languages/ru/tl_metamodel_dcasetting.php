a6a0c0ef11bafece25cc9a7dc7b2ab576f6515bd
Mini Model <minimodel@metamodel.me>
2014-07-29 20:14:49 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_tags/languages/ru/tl_metamodel_dcasetting.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-10-26T04:11:23+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard']['0'] = 'Показать как мастер чекбоксов';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard']['1'] = 'Выберите этот параметр для изменения типа представления с «чекбокс» на «мастер чекбоксов».';
+
