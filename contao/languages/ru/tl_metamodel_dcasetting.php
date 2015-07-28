ce4bd98c174aa809d75135f5eea6d850c73327e1
Mini Model <minimodel@metamodel.me>
2015-07-28 16:19:33 +0200

    Updated translations from transifex.

--- contao/languages/ru/tl_metamodel_dcasetting.php
+++ contao/languages/ru/tl_metamodel_dcasetting.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-13T12:14:33+02:00
+ * last-updated: 2015-07-13T16:14:13+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard']['0']           = 'Показать как мастер чекбоксов';
@@ -17,4 +17,8 @@ $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard_reference']['0'] =
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard_reference']['1'] = 'Показать как мастер чекбоксов';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard_reference']['2'] = 'Показать как всплывающее окно выбора';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard_reference']['3'] = 'Показать как список тегов';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_maxLevel']['0']            = 'Максимальный уровень в дереве выбора';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_maxLevel']['1']            = 'Если передается значение >0, элемент выше этого уровня не будет выбран.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_minLevel']['0']            = 'Минимальный уровень в дереве выбора';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_minLevel']['1']            = 'Если передается значение >0, элемент ниже этого уровня не будет выбран.';
 
